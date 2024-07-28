46785fc5979848f174fea4b3207d075fb01c7906
Abhishek Kank <abhishekkank5@gmail.com>
2024-07-28 10:23:34 +0530

    Frontend Layout SetUp

---
+++ Frontend/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className="header"style={{minHeight:'10vh'}}>
+          <h1>Header</h1>
+    </div>
+
+
+    </>
+  )
+}
+
+export default Header
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import './index.css'
+import Layout from '../components/Layout'
 import './App.css'
 
+
+
+
 function App() {
-  
 
   return (
     <>
-    <h1 className="text-3xl text-center justify-center font-bold">
-      FrontEnd
-    </h1>     
+    <Layout>
+              <div class="text-center m-0 p-0">
+            <div class="row w-100">
+              <div class="col-4 logoSection">col-4</div>
+              <div class="col-4 wholeForm form1">col-4</div>
+              <div class="col-4 wholeForm form2">col-4</div>
+            </div>
+          </div>
+    </Layout>     
     </>
   )
 }
